58b510fb727bd000778fd8c33fbc44dd5dc4de88
ZMBNJNM <58258293+kamileqq@users.noreply.github.com>
2022-04-05 18:46:58 +0200

    ee

--- ProjectLighthouse/Pages/Layouts/BaseLayout.cshtml.cs
+++ ProjectLighthouse/Pages/Layouts/BaseLayout.cshtml.cs
@@ -13,11 +13,9 @@ public class BaseLayout : PageModel
     public readonly List<PageNavigationItem> NavigationItems = new()
     {
         new PageNavigationItem("Home", "/", "home"),
-        new PageNavigationItem("Users", "/users/0", "users"),
+        new PageNavigationItem("Users", "/users/0", "user friends"),
         new PageNavigationItem("Photos", "/photos/0", "camera"),
         new PageNavigationItem("Levels", "/slots/0", "certificate"),
-        new PageNavigationItem("Rules", "/rules", "info"),
-		new PageNavigationItem("Donation", "/donate", "dollar"),
     };
 
     public readonly List<PageNavigationItem> NavigationItemsRight = new();
